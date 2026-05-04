14593f9dbb955beb2834236e2912c9698bacc1ca
6730611077-sudo <6730611077@psu.ac.th>
2026-05-04 15:19:00 +0700

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,11 +1,10 @@
-// โค้ดสำหรับไฟล์ models/db.js
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root', // ปกติ XAMPP จะใช้ root
-    password: '', // ปกติ XAMPP รหัสผ่านจะว่างเปล่า
-    database: 'supermarket_db' // ชื่อฐานข้อมูลที่เราสร้างไว้
+    user: 'root', 
+    password: '', 
+    database: 'supermarket_db' 
 });
 
 module.exports = pool.promise();
